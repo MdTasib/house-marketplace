cec6ce0d0d7f56c647ad1b7180577ebb77424027
MdTasib <ohidulalam442@gmail.com>
2022-07-02 12:12:30 +0600

    route setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import SingIn from "./pages/Singin";
+import SingUp from "./pages/Singup";
+import ForgetPassword from "./pages/ForgetPassword";
+
 function App() {
 	return (
 		<div>
 			<h3>hello</h3>
+			<Routes>
+				<Route path='/' element={<Explore />} />
+				<Route path='/offers' element={<Offers />} />
+				<Route path='/singin' element={<SingIn />} />
+				<Route path='/singup' element={<SingUp />} />
+				<Route path='/forget-password' element={<ForgetPassword />} />
+			</Routes>
 		</div>
 	);
 }
