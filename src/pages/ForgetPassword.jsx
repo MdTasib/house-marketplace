55450a7894365ea460bb30a6175931cd67f234b3
MdTasib <ohidulalam442@gmail.com>
2022-07-02 11:57:37 +0600

    page folder structure

---
+++ src/pages/ForgetPassword.jsx
@@ -0,0 +1,9 @@
+function ForgetPassword() {
+  return (
+    <div>
+      Forget Password
+    </div>
+  );
+}
+
+export default ForgetPassword;
---
+++ src/pages/Singin.jsx
@@ -0,0 +1,9 @@
+function SingIn() {
+  return (
+    <div>
+      Sing In
+    </div>
+  );
+}
+
+export default SingIn;
