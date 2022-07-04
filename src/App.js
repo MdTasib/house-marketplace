83ff986d934c203a258948e08d1ba769911056a3
MdTasib <ohidulalam442@gmail.com>
2022-07-04 10:33:56 +0600

    sign in

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
 import SignIn from "./pages/Signin";
 import SignUp from "./pages/Signup";
+import Profile from "./pages/Profile";
 import ForgetPassword from "./pages/ForgetPassword";
 import Navbar from "./components/Navbar";
 
@@ -12,7 +13,7 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Explore />} />
 				<Route path='/offers' element={<Offers />} />
-				<Route path='/profile' element={<SignIn />} />
+				<Route path='/profile' element={<Profile />} />
 				<Route path='/sign-in' element={<SignIn />} />
 				<Route path='/sign-up' element={<SignUp />} />
 				<Route path='/forget-password' element={<ForgetPassword />} />
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,15 @@
+import { getAuth } from "firebase/auth";
+import { useEffect, useState } from "react";
+
+
 function Profile() {
-  return (
-    <div>
-      Profile
-    </div>
-  );
+  const [user, setUser] = useState(null);
+  const auth = getAuth();
+  useEffect(() => {
+    setUser(auth.currentUser);
+  }, [])
+
+  return user ? <h3>{user.displayName}</h3> : 'Not Found'
 }
 
 export default Profile;
