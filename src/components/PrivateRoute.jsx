554d496242cd4203f7f25c9dcf12af978e3daaa3
MdTasib <ohidulalam442@gmail.com>
2022-07-13 11:25:46 +0600

    OAuth

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,15 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAuthStatus } from '../hooks/useAuthStatus'
+import Spinner from './Spinner'
 
+const PrivateRoute = () => {
+  const { loggedIn, checkingStatus } = useAuthStatus()
 
-function PrivateRoute() {
-  const loggedIn = false;
+  if (checkingStatus) {
+    return <Spinner />
+  }
 
-  return loggedIn ? <Outlet/> : <Navigate to='/sign-up'/>
+  return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />
 }
 
-export default PrivateRoute;
+export default PrivateRoute
--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,5 +1,26 @@
-import React from "react";
+import { useEffect, useState, useRef } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-export function useAuthStatus() {
-	return <div></div>;
-}
+export const useAuthStatus = () => {
+	const [loggedIn, setLoggedIn] = useState(false);
+	const [checkingStatus, setCheckingStatus] = useState(true);
+	const isMounted = useRef(true);
+
+	useEffect(() => {
+		if (isMounted) {
+			const auth = getAuth();
+			onAuthStateChanged(auth, user => {
+				if (user) {
+					setLoggedIn(true);
+				}
+				setCheckingStatus(false);
+			});
+		}
+
+		return () => {
+			isMounted.current = false;
+		};
+	}, [isMounted]);
+
+	return { loggedIn, checkingStatus };
+};
